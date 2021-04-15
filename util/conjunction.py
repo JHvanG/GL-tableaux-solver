9dee5d07e7e8366ec63050debc011141a3fda839
Jeroen <jeroen.h.vangelder@gmail.com>
2021-04-15 22:51:32 +0200

    further work on generator

---
+++ util/conjunction.py
@@ -0,0 +1,3 @@
+class Conjunction(Connective):
+    def __init__(self, character, binary = True, formula_one, formula_two = None):
+        Connective.__init__(character, binary, formula_one, formula_two)
--- util/connective.py
+++ util/connective.py
@@ -1,10 +1,27 @@
 class Connective(object):
-    def __init__(self, binary = True, formula_one, formula_two = None):
+    def __init__(self, character, binary = True, formula_one, formula_two = None):
+        # character representing connective
+        self.character = character
         # boolean for binary connective
         self.binary = binary
         # connected formulae
         self.formula_one = formula_one
         self.formula_two = formula_two
+
+    def get_character(self):
+        return self.character
+
+    def get_binary(self):
+        return self.binary
+
+    def get_formula_one(self):
+        return self.formula_one
+
+    def get_formula_two(self):
+        return self.formula_two
+
+    # each connective has a get next connective method
+    # all gets are inherited from here
 
     def find_unique(self):
         
