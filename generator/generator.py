7fc95d6cc969b1fe008a611fbd89585b8a31b658
Jeroen <jeroen.h.vangelder@gmail.com>
2021-04-19 22:58:21 +0200

    again, nothing noteworthy

--- generator/generator.py
+++ generator/generator.py
@@ -1,6 +1,12 @@
 class Generator(object):
     def __init__(self):
-        #vars for init
+        while True:
+            self.create_formula()
+
+    def create_formula(self):
+        # this function will generate a new formula of same length as last unless all are already present
+        # then it will increase in length and check for all connectives again
+        # first only with 1 letter
 
 if __name__ == "__main__":
     generator = Generator()
