aef530b23af03662ddaf0fd0fb2984fd414e1a05
Jeroen <jeroen.h.vangelder@gmail.com>
2021-04-19 21:52:59 +0200

    nothing done

--- util/conjunction.py
+++ util/conjunction.py
@@ -1,3 +1,6 @@
 class Conjunction(Connective):
     def __init__(self, character, binary = True, formula_one, formula_two = None):
         Connective.__init__(character, binary, formula_one, formula_two)
+
+    def branch(self):
+        # add two formula instances to current branch
---
+++ util/formula.py
@@ -0,0 +1,5 @@
+class Formula(object):
+    def __init__(self, is_atom, connective = None, formula_one, formula_two = None):
+        # boolean for atom
+        # connective
+        # components --> generates new connective object
