aa60a7f13245a874d3e62941fc9597de7a624eb5
Jeroen <63635197+JHvanG@users.noreply.github.com>
2021-05-07 16:20:56 +0200

    Change of plans!

--- src/generator.py
+++ src/generator.py
@@ -7,15 +7,20 @@ class Generator(object):
     def __init__(self):
         self.total_length = 1
         self.position = 0
-        self.intermediate_formula = None
-        self.resulting_formula = None
+        self.generator_on = False
+        # self.intermediate_formula = None
+        # self.resulting_formula = None
+
+    def set_generator_on(self, value):
+        self.generator_on = value
 
     def get_total_length(self):
         return self.total_length
 
     def set_total_length(self, new_length):
         self.total_length = new_length
-
+    '''
+    # TODO: replace this method with new logic
     def create_formula(self, formula_length):
         if formula_length == 1:
             # TODO: have more than one letter
@@ -46,6 +51,8 @@ class Generator(object):
 
                 len_one -= 1
                 len_two += 1
+                
+    '''
 
     # this method is responsible for keeping the resulting formula up to date
     def update_resulting_formula(self, filler):
@@ -66,25 +73,34 @@ class Generator(object):
                 for unary connectives this should only remove itself if its formula is None
             """
 
-    # this method removes the last element in depth first manner
-    def remove_element(self, formula):
-        # remove last element of binary connective
-        # else remove first element
-        # else remove entire connective
-        # do this in a depth first manner
-
-        if formula.binary and not formula.get_formula_two() is None:
-            # check formula
+    # This method is responsible for saving the formulas a .form file using pickle
+    def save_to_file(self, formula):
 
 
-        
+    # This method will produce and save the two atoms A and B
+    def generate_atoms(self):
 
-    def start(self):
-        while True:
-            self.create_formula(self.total_length)
-            self.total_length += 1
+
+    # This method will produce and save combinations of the previously saved formulas
+    def generate_combinations(self, formula_complexity):
+
+
+    # TODO: Fix method to procedurally generate increasing combinations of past formulae.
+    #       Using pickle save past formulae
+    def create_formula(self):
+        formula_complexity = 0
+
+        while self.generator_on:
+            if formula_complexity == 0:
+                self.generate_atoms()
+            else:
+                self.combine_formulas(formula_complexity)
+
+            formula_complexity += 1
+
 
 
 if __name__ == "__main__":
     generator = Generator()
-    generator.start()
+    generator.set_generator_on(True)
+    generator.create_formula()
