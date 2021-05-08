640a6ea39fc2e1b6ded5388b2f442cca1b8a5b06
Jeroen <63635197+JHvanG@users.noreply.github.com>
2021-05-08 22:42:44 +0200

    read write works

--- src/generator.py
+++ src/generator.py
@@ -1,7 +1,8 @@
-import pickle
 from util import negation, conjunction
 from util import formula
 from util import connective_enum
+from pathlib import Path
+import pickle, os
 
 
 class Generator(object):
@@ -9,8 +10,8 @@ class Generator(object):
         self.total_length = 1
         self.position = 0
         self.generator_on = False
-        # self.intermediate_formula = None
-        # self.resulting_formula = None
+        self.formula_complexity = 0
+        self.storage_path = os.path.join(Path(__file__).parents[1], "storage")
 
     def set_generator_on(self, value):
         self.generator_on = value
@@ -20,40 +21,6 @@ class Generator(object):
 
     def set_total_length(self, new_length):
         self.total_length = new_length
-    '''
-    # TODO: replace this method with new logic
-    def create_formula(self, formula_length):
-        if formula_length == 1:
-            # TODO: have more than one letter
-            self.position += 1
-            self.update_resulting_formula(formula.Formula(None, "A", None, True, False))
-            self.check_complete()
-            self.position -= 1
-        elif formula_length == 2:
-            # TODO: also add box and diamond based on what has been used using the ENUM (+1)
-            self.position += 2
-            self.update_resulting_formula(negation.Negation("A"))
-            self.check_complete()
-            self.position -= 2
-        else:
-            # TODO: make sure any connective can be used here
-            len_one = formula_length - 2
-            len_two = formula_length - len_one - 1
-
-            while len_one >= 1:
-                print("Hello")
-                # note that the first AND second formulas are set to None, these are filled in recursively
-                self.update_resulting_formula(conjunction.Conjunction(None, None))
-                # TODO: this currently assumes a binary connective is used!!!
-                self.create_formula(len_one)
-                self.position += (formula_length - len_two)
-                self.create_formula(len_two)
-                self.position -= (formula_length - len_two)
-
-                len_one -= 1
-                len_two += 1
-                
-    '''
 
     # this method is responsible for keeping the resulting formula up to date
     def update_resulting_formula(self, filler):
@@ -75,31 +42,54 @@ class Generator(object):
             """
 
     # This method is responsible for saving the formulas a .form file using pickle
-    def save_to_file(self, formula):
+    def save_to_file(self, formula_list):
+        filepath = os.path.join(self.storage_path, "formula_set_" + str(self.formula_complexity) + ".form")
+        with open(filepath, 'wb') as file:
+            for form in formula_list:
+                pickle.dump(form, file, pickle.HIGHEST_PROTOCOL)
 
+    def read_from_file(self):
+        filepath = os.path.join(self.storage_path, "formula_set_" + str(self.formula_complexity) + ".form")
+        with open(filepath, 'rb') as file:
+            while True:
+                try:
+                    print(pickle.load(file))
+                except EOFError:
+                    break
 
     # This method will produce and save the two atoms A and B
     def generate_atoms(self):
         atom_one = formula.Formula(None, "A", None, True, False)
         atom_two = formula.Formula(None, "B", None, True, False)
+        formula_list = [atom_one, atom_two]
+        self.save_to_file(formula_list)
 
     # This method will produce and save combinations of the previously saved formulas
-    def generate_combinations(self, formula_complexity):
-
+    def generate_combinations(self):
+        if self.formula_complexity == 1:
+            ''' 
+        Complexity 1:
+            - Unary connectives ~, [] and <> on complexity 0
+        Complexity 2:
+            - Binary connectives &, |, -> and <-> on complexity 0
+            - Unary connectives ~, [] and <> on complexity 1
+        Complexity 3:
+            - Binary connectives on complexity combination of 0 with 2 and combination of 1 with 1
+            - Unary connectives on binary connectives and single unary connectives of complexity 2        
+        '''
 
     # TODO: Fix method to procedurally generate increasing combinations of past formulae.
     #       Using pickle save past formulae
     def create_formula(self):
-        formula_complexity = 0
+        self.generate_atoms()
+        self.read_from_file()
+        #while self.generator_on:
+        #    if self.formula_complexity == 0:
+        #        self.generate_atoms()
+        #    else:
+        #        self.generate_combinations()
 
-        while self.generator_on:
-            if formula_complexity == 0:
-                self.generate_atoms()
-            else:
-                self.combine_formulas(formula_complexity)
-
-            formula_complexity += 1
-
+            #self.formula_complexity += 1
 
 
 if __name__ == "__main__":
